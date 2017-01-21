43ba6daa76da54f2402867fe9dae6b6d5efc199f
rnoth <moth@airmail.cc>
2017-01-21 14:05:33 -0800

    This should have been made portable.

--- iimenu.sh
+++ iimenu.sh
@@ -1,4 +1,4 @@
-#!/bin/ksh
+#!/bin/sh
 
 dmenu=''
 dir=/tmp
@@ -9,10 +9,10 @@ channels=*/*/out
 chanstat=$(stat -c "%Y %n" $channels)
 
 echo $chanstat
-selserv=$(<<< "$chanstat" cut -d"/" -f 1 | sort -u -k2 | cut -d" " -f 2 | dmenu $dmenu -p "Server:")
+selserv=$(echo "$chanstat" | cut -d"/" -f 1 | sort -u -k2 | cut -d" " -f 2 | dmenu $dmenu -p "Server:")
 test -z "$selserv" && exit 1
 
-selchan=$(<<< "$chanstat" grep "$selserv" | sort -r | cut -d"/" -f 2 | grep -Ev "(^irc$|^global$|^nickserv$|^py-ctcp$)" | dmenu $dmenu -p "Channel:")
+selchan=$(echo "$chanstat" | grep "$selserv" | sort -r | cut -d"/" -f 2 | grep -Ev "(^irc$|^global$|^nickserv$|^py-ctcp$)" | dmenu $dmenu -p "Channel:")
 test -z "$selchan" && exit 1
 
 while 	text=$(</dev/null dmenu -p "${selchan}" \
